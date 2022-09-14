2e178c72d67248122bdc31fda1e7b7c2e818be0d
Ben Whitfield <bpwhitfield@outlook.com>
2022-09-14 17:50:54 +0100

    Add dockerfile

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from website import create_app
+import os
 
 app = create_app()
 
 # Only execute if we run this file 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
